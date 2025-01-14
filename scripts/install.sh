7f686bc4b360a94b9b554de01929ccfbc19c5ba6
Minuit-Ferina <124693912+Minuit-Ferina@users.noreply.github.com>
2025-01-14 18:12:20 +0100

    Update install.sh
    Update build.yml

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,12 +3,12 @@ cd opt/bin
 curl -L -o LSL-PyOptimizer.zip https://github.com/Sei-Lisa/LSL-PyOptimizer/archive/refs/heads/master.zip
 unzip -o LSL-PyOptimizer
 mv LSL-PyOptimizer-master LSL-PyOptimizer
-cd ..
+cd ../..
 
 mkdir -p opt/include
 cd opt/include
 curl -L -o SetLinkTextureFast.lsl https://raw.githubusercontent.com/Minuit-Ferina/SetLinkTextureFast.lsl/refs/heads/main/SetLinkTextureFast.lsl
 curl -L -o PrimLinkFinder.lsl  https://raw.githubusercontent.com/Minuit-Ferina/PrimLinkFinder.lsl/refs/heads/main/PrimLinkFinder.lsl
 curl -L -o PrimLinkFinder.lsl  https://raw.githubusercontent.com/Minuit-Ferina/PrimLinkFinder.lsl/refs/heads/main/PrimLinkFinder.lsl
-cd ..
+cd ../..
 
